6690dbf096c51568280f86c35bc96df38a928f65
eranelkin <eran.elkin@gmail.com>
2022-09-01 18:49:41 +0300

    fixed ts

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
